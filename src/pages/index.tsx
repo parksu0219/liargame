799b4d90c417b09173d4dab92e3711e234d65c9d
[parksu0219] <s.park@leihauoli.com>
2020-10-04 17:55:36 +0900

    fix: error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,11 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import * as ReactDOM from "react-dom";
 import App from '../components/App';
+import { Router, RouteComponentProps } from "@reach/router"
+// import { BrowserRouter as Router, Route } from "";
 import { Provider } from "react-redux";
 import { createStore } from 'redux';
+import Main from "../components/Main";
 import rootReducer from '../modules';
 import './index.css';
 
@@ -15,9 +18,16 @@ const store = createStore(rootReducer);
 
 const IndexPage = () => (
   <Provider store={store}>
-    tast
+    <Router>
+    <RouterPage path="/" pageComponent={<Main />} />
+    </Router>
         {/* <App /> */}
     </Provider>
 )
 
 export default IndexPage
+
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
