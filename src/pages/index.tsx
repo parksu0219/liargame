8954782131027020154ea3a1bab5b49eac1dc0c4
[parksu0219] <s.park@leihauoli.com>
2020-10-04 18:04:35 +0900

    add: style add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,37 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { FunctionComponent } from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import * as ReactDOM from "react-dom";
-import App from '../components/App';
-import { Router, RouteComponentProps } from "@reach/router"
+import styled from "styled-components";
+import App from "../components/App";
+import { Router, RouteComponentProps } from "@reach/router";
 // import { BrowserRouter as Router, Route } from "";
 import { Provider } from "react-redux";
-import { createStore } from 'redux';
+import { createStore } from "redux";
 import Main from "../components/Main";
-import rootReducer from '../modules';
-import './index.css';
+import rootReducer from "../modules";
+import "./index.css";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer)
+
+type Props = { component: FunctionComponent } & RouteComponentProps;
+
+const Route = ({ component: Component, ...rest }) => (
+  <Component {...rest} />
+);
 
 const IndexPage = () => (
   <Provider store={store}>
-    aaaaaaa
+    <StyledLiarButtonWrapper>aaaaaaa</StyledLiarButtonWrapper>
     <Router>
-      
-    <RouterPage path="/" pageComponent={<Main />} />
+      <Route path="/" component={<Main />} />
     </Router>
-        {/* <App /> */}
-    </Provider>
+    {/* <App /> */}
+  </Provider>
 )
 
-export default IndexPage
+export default IndexPage;
 
 
-const RouterPage = (
-  props: { pageComponent: JSX.Element } & RouteComponentProps
-) => props.pageComponent;
+const StyledLiarButtonWrapper = styled.div`
+  background-color: red;
+`;
