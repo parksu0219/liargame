bed1c666ad3e792c1b3df8cafef1ba16a9a025f7
[parksu0219] <s.park@leihauoli.com>
2020-10-04 16:03:46 +0900

    fix: test1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>helloaaaa</h1>
+    <h1>helloaaaaaaa</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
