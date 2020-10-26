56c68b0b36d058b499d4c391d6b056e5c7f8bf94
PARK <s.park@leihauoli.com>
2020-10-26 11:03:10 +0900

    fix: firebase analytics추가

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,31 +1,7 @@
 import firebase from "gatsby-plugin-firebase"
 
-
-// // firebaseConfig
-// const config = {
-//   apiKey: "AIzaSyBjZzldlYtV8HpdEgWH6-v-4tNxrcYbnK8",
-//   authDomain: "liar-game-85144.firebaseapp.com",
-//   databaseURL: "https://liar-game-85144.firebaseio.com",
-//   projectId: "liar-game-85144",
-//   storageBucket: "liar-game-85144.appspot.com",
-//   messagingSenderId: "847081075490",
-//   appId: "1:847081075490:web:959ea4ec6adb2aba8fab7b",
-//   measurementId: "G-F002Y7MVLP",
-// };
-
-// firebase.initializeApp(config);
-
-
-
+firebase.analytics();
 
 export const remoteConfig = firebase.remoteConfig();
-
-
-
-
-
-
-// export const auth = firebase.auth();
-
 
 export default firebase;
